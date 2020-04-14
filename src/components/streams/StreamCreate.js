d9c65a7b339bcc82b85415b4cc9d0889cbbf9450
arafat-213 <tai.arafat.at@gmail.com>
2020-04-14 11:10:18 +0530

    added redux-form for create stream

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,80 @@
-import React from "react"
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-	return <div>StreamCreate</div>
+class StreamCreate extends Component {
+	/**
+	 *
+	 * @param {erorr, touched} meta
+	 */
+	renderError({ error, touched }) {
+		if (touched && error)
+			return (
+				<div className="ui error message">
+					<div className="header">{error}</div>
+				</div>
+			)
+	}
+
+	/**
+	 *
+	 * @param {input}formProps Passed by <Field/>, contains various input element attributes
+	 * @returns an Input element for form
+	 */
+	renderInput = ({ input, label, meta }) => {
+		const classes = `field ${meta.error && meta.touched ? 'error' : ''}`
+		return (
+			<div className={classes}>
+				<label>{label}</label>
+				<input {...input} />
+				{this.renderError(meta)}
+			</div>
+		)
+	}
+
+	/**
+	 * @handler for form submit evetn
+	 * Is fired whenever the redux-form is submitted
+	 * @param formValues event-args
+	 */
+	onSubmit(formValues) {
+		console.log(formValues)
+	}
+
+	render() {
+		return (
+			<div>
+				<form
+					onSubmit={this.props.handleSubmit(this.onSubmit)}
+					className="ui form error"
+				>
+					<Field
+						name="title"
+						component={this.renderInput}
+						label="Enter Title"
+					/>
+					<Field
+						name="description"
+						component={this.renderInput}
+						label="Enter Description"
+					/>
+					<button className="ui button primary">Submit</button>
+				</form>
+			</div>
+		)
+	}
 }
 
-export default StreamCreate
+const validate = formValues => {
+	const errors = {}
+	if (!formValues.title) {
+		errors.title = 'Title can not be empty'
+	}
+	if (!formValues.description) {
+		errors.description = 'Ddescription can not be empty'
+	}
+	return errors
+}
+export default reduxForm({
+	form: 'streamCreate',
+	validate,
+})(StreamCreate)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
-import { combineReducers } from "redux"
-import authReducer from "./authReducer"
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import authReducer from './authReducer'
 
+/**
+ * @field auth Authentication reducer => Maintain authentication state
+ * @field form Form reducer from redux-form library => Maintains form elements
+ */
 export default combineReducers({
 	// TODO: Add the reducers here
 	auth: authReducer,
+	form: formReducer,
 })
