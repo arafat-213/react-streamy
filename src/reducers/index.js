0d055b88effeec75258861a4811ffd901b87dec9
arafat-213 <tai.arafat.at@gmail.com>
2020-04-15 12:25:50 +0530

    added all the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
-import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-import authReducer from './authReducer'
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 /**
  * @field auth Authentication reducer => Maintain authentication state
  * @field form Form reducer from redux-form library => Maintains form elements
  */
 export default combineReducers({
-	// TODO: Add the reducers here
-	auth: authReducer,
-	form: formReducer,
-})
+  // TODO: Add the reducers here
+  auth: authReducer,
+  form: formReducer,
+  streams: streamReducer,
+});
