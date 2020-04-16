e61c68f1a34f6a444a941a6eed6e66f3352538ee
arafat-213 <tai.arafat.at@gmail.com>
2020-04-16 11:41:44 +0530

    added create/edit/delete stream button

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,71 +1,71 @@
-import streams from '../apis/streams'
-import { 
-	SIGN_IN, 
-	SIGN_OUT, 
-	CREATE_STREAM,
-	FETCH_STREAM,
-	FETCH_STREAMS,
-	EDIT_STREAM,
-	DELETE_STREAM
- } from './types'
+import streams from '../apis/streams';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from './types';
 
-export const signIn = (userId) => {
-	return {
-		type: SIGN_IN,
-		payload: userId,
-	}
-}
+export const signIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
 
 export const signOut = () => {
-	return {
-		type: SIGN_OUT,
-	}
-}
+  return {
+    type: SIGN_OUT,
+  };
+};
 
 /* AC for creating a stream */
-export const createStream = formValues => async dispatch  => {
-	const response = await streams.post('/streams', formValues)
-	dispatch({
-		type: CREATE_STREAM,
-		payload: response.data
-	})
-}
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
+  dispatch({
+    type: CREATE_STREAM,
+    payload: response.data,
+  });
+};
 
 /* AC for listing the streams */
 export const fetchStreams = () => async dispatch => {
-	const response = await streams.get('/streams')
-	dispatch({
-		type: FETCH_STREAMS,
-		payload: response.data
-	})
-}
+  const response = await streams.get('/streams');
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data,
+  });
+};
 
 /* AC for fetching a single stream by ID */
 export const fetchStream = id => async dispatch => {
-	const response = await streams.get(`/stream/${id}`)
-	dispatch({
-		type: FETCH_STREAM,
-		payload: response.data
-	})
-}
+  const response = await streams.get(`/stream/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data,
+  });
+};
 
 /* AC for editing a stream */
 export const editStream = (id, updatedValues) => async dispatch => {
-	const response = await streams.put(`/streams/${id}`, updatedValues)
+  const response = await streams.put(`/streams/${id}`, updatedValues);
 
-	dispatch({
-		type: EDIT_STREAM,
-		payload: response.data
-	})
-}
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data,
+  });
+};
 
 /*AC for deleting a stream */
 export const deleteStream = id => async dispatch => {
-	await streams.delete(`/streams/${id}`)
+  await streams.delete(`/streams/${id}`);
 
-	dispatch({
-		type: DELETE_STREAM,
-		payload: id
-	})
-
-}
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id,
+  });
+};
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
-
+import { Link } from 'react-router-dom';
 class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
+  renderDeleteEditButton(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderDeleteEditButton(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -20,11 +31,25 @@ class StreamList extends Component {
       );
     });
   }
+
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateButton()}
       </div>
     );
   }
@@ -32,7 +57,9 @@ class StreamList extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    currentUserId: state.auth.userId,
     streams: Object.values(state.streams),
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
