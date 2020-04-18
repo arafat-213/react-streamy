869c7ec85e672e1361268650620c60f03abab1a5
arafat-213 <tai.arafat.at@gmail.com>
2020-04-18 16:31:00 +0530

    added video player, not functional yet

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 
 class StreamShow extends Component {
-	componentDidMount() {
-		this.props.fetchStream(this.props.match.params.id)
+	constructor(props) {
+		super(props)
+		this.videoRef = React.createRef()
 	}
+
+	componentDidMount() {
+		const { id } = this.props.match.params
+		this.props.fetchStream(id)
+		this.buildPlayer()
+	}
+
+	componentDidUpdate() {
+		this.buildPlayer()
+	}
+
+	buildPlayer() {
+		if (this.flvPlayer || !this.props.stream) {
+			return
+		}
+		const { id } = this.props.match.params
+		this.flvPlayer = flv.createPlayer({
+			type: 'flv',
+			url: `http://localhost:8000/live/${id}.flv`
+		})
+		this.flvPlayer.attachMediaElement(this.videoRef.current)
+		this.flvPlayer.load()
+	}
+
 	render() {
 		if (!this.props.stream) return <div>Loading...</div>
 		const { title, description } = this.props.stream
 		return (
 			<div>
+				<video
+					ref={this.videoRef}
+					style={{ width: '100%' }}
+					controls={true}
+				/>
 				<h1>{title}</h1>
 				<h5> {description}</h5>
 			</div>
